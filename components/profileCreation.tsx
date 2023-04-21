6ab8559118217518100877e7c3a87af82e27657b
Brennin <bkjoiner1@gmail.com>
2023-04-21 19:51:40 -0400

    add dev buttons for component navigation

---
+++ components/profileCreation.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+function ProfileCreation() {
+  return (
+    <>
+      PROFILE CREATION
+    </>
+
+  );
+}
+
+export default ProfileCreation;
