4a0080f42a222c6df478d74b113d20ad8fdeb9cf
Zach Timmerman <zachary.timmerman@gmail.com>
2023-04-21 16:55:49 -0400

    Create API route skeleton and database config

---
+++ server/controllers/messagesController.js
@@ -0,0 +1,13 @@
+let Models = require('../Models');
+
+const messagesController = {
+  get: (req, res) => {
+    // Models.messages.getAll()
+      // .then ...
+  },
+  getOne: (req, res) => {
+    //
+  }
+}
+
+module.exports = messagesController;
---
+++ server/routes.js
@@ -0,0 +1,12 @@
+const controller = require('./controllers');
+const router = require('express').Router();
+
+// router.get('/', (req, res) => {
+//   console.log('test');
+//   res.end('test');
+// });
+
+router.get('/messages', controller.messages.get);
+router.get('/messages/:id', controller.messages.getOne);
+
+module.exports = router;
