a1e19ae4fea5adcff9670f62a27108bef7424cfc
Zach Timmerman <zachary.timmerman@gmail.com>
2023-04-21 19:54:51 -0400

    Adjust schema

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -4,7 +4,9 @@ CREATE DATABASE hire;
 \c hire;
 
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  s_id INT NOT NULL
 );
 
 CREATE TABLE messages (
@@ -15,3 +17,27 @@ CREATE TABLE messages (
   timestamp TIMESTAMP DEFAULT NOW()
 );
 
+CREATE TABLE business_proposals (
+  id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL REFERENCES users(id),
+  headline TEXT NOT NULL,
+  overview TEXT NOT NULL,
+  skills TEXT NOT NULL,
+  estimated_timeline TEXT NOT NULL,
+  location TEXT NOT NULL,
+  budget INT NOT NULL,
+  timestamp TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE freelancers (
+  id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL REFERENCES users(id),
+  role TEXT NOT NULL,
+  rate TEXT NOT NULL,
+  work_history TEXT NOT NULL,
+  skills TEXT NOT NULL,
+  location TEXT NOT NULL,
+  education TEXT NOT NULL,
+  portfolio TEXT NOT NULL,
+  timestamp TIMESTAMP DEFAULT NOW()
+)
