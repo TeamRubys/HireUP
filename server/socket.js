6b917d85c0a337ab398ee04aed7ed3ed95167ea2
Brennin <bkjoiner1@gmail.com>
2023-04-22 16:47:00 -0400

    add join room, and send message routes for socket server

--- server/socket.js
+++ server/socket.js
@@ -6,6 +6,14 @@ module.exports = (server) => {
   io.on('connection', (socket) => {
     console.log('A user connected');
 
+    socket.on('joinRoom', (roomName) => {
+      socket.join(roomName)
+    });
+
+    socket.on('sendMessage', ({roomName, message}) => {
+      io.to(roomName).emit('newMessage', message)
+    })
+
     socket.on('disconnect', () => {
       console.log('A user disconnected');
     });
