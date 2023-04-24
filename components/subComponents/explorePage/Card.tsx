2271e23b86fe37d6a3dae38c762d1729de5d42fd
phaeq <johnnyyin0@gmail.com>
2023-04-24 17:09:46 -0400

    Mostly done with client side explore page

--- components/subComponents/explorePage/Card.tsx
+++ components/subComponents/explorePage/Card.tsx
@@ -1,21 +1,18 @@
 function Card({}) {
     return (
-      <div className="border p-6 rounded-lg">
+      <div className="border p-6 rounded-lg mb-10">
         <div id="card-header" className="flex justify-between items-center mb-10">
             <div className="flex flex-col">
-            <h2 className="text-2xl font-bold">Example Title</h2>
-            <ul>{/*client*/}</ul>
-            <ul>{/*timestamp*/}</ul>
+            <h2 className="text-2xl font-bold">Example Headline</h2>
+            <ul className="">Client</ul>
+            <ul className="text-gray-400">Date Posted</ul>
             </div>
         <div>
-          <button className="mr-2 bg-white hover:bg-blue-100 text-black font-bold py-2 px-4 rounded border border-gray-400">
+          <button className="mb-9 mr-2 bg-white hover:bg-blue-100 text-black font-bold py-2 px-4 rounded border border-gray-400">
             Save
           </button>
-          <button className="mr-2 bg-white hover:bg-blue-100 text-black font-bold py-2 px-4 rounded border border-gray-400">
+          <button className="bg-white hover:bg-blue-100 text-black font-bold py-2 px-4 rounded border border-gray-400">
             Message
-          </button>
-          <button className="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded border border-gray-400">
-            Apply
           </button>
         </div>
         </div>
@@ -23,7 +20,16 @@ function Card({}) {
             <li className="mb-2 font-bold" >Overview: </li>{}
             <li className="mb-2 font-bold" >Skills required:</li>{}
         </ul>
-        <div>Role: {} | Location: {} | Pay: {} </div>
+        <div className="flex justify-between items-center border p-2 rounded-lg">
+            <div>
+            <span className="font-bold mr-2 ml-2">Role</span>
+            <span className="mr-2">Location</span>
+            <span>Budget</span>
+            </div> 
+        <button className="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded border border-gray-400">
+            Apply
+          </button>
+        </div>
       </div>
     );
   }
--- components/subComponents/explorePage/CardList.tsx
+++ components/subComponents/explorePage/CardList.tsx
@@ -3,7 +3,8 @@ import Card from "./Card"
 function CardList({}) {
   return (
     <div>
-      <Card />
+      <Card/>
+      <Card/>
       <div className="flex justify-center mt-10">
         <button className="bg-black hover:bg-blue-700 text-white font-bold py-3 px-10 rounded">
           Load More
