a642c1b0b5455a81f99f3ca2e9455e5d51c605f4
phaeq <johnnyyin0@gmail.com>
2023-04-24 16:36:20 -0400

    Added loadmore button, fixed some styling on card

--- components/subComponents/explorePage/Card.tsx
+++ components/subComponents/explorePage/Card.tsx
@@ -1,7 +1,12 @@
 function Card({}) {
     return (
-      <div className="border p-4 rounded-lg flex justify-between items-center">
-        <h2 className="text-2xl font-bold">Example Proposal</h2>
+      <div className="border p-6 rounded-lg">
+        <div id="card-header" className="flex justify-between items-center mb-10">
+            <div className="flex flex-col">
+            <h2 className="text-2xl font-bold">Example Title</h2>
+            <ul>{/*client*/}</ul>
+            <ul>{/*timestamp*/}</ul>
+            </div>
         <div>
           <button className="mr-2 bg-white hover:bg-blue-100 text-black font-bold py-2 px-4 rounded border border-gray-400">
             Save
@@ -13,6 +18,12 @@ function Card({}) {
             Apply
           </button>
         </div>
+        </div>
+        <ul>
+            <li className="mb-2 font-bold" >Overview: </li>{}
+            <li className="mb-2 font-bold" >Skills required:</li>{}
+        </ul>
+        <div>Role: {} | Location: {} | Pay: {} </div>
       </div>
     );
   }
--- components/subComponents/explorePage/CardList.tsx
+++ components/subComponents/explorePage/CardList.tsx
@@ -1,10 +1,16 @@
 import Card from "./Card"
-function CardList({}){
-    return(
-        <div>
-        <Card/>
-        </div>
-    )
+
+function CardList({}) {
+  return (
+    <div>
+      <Card />
+      <div className="flex justify-center mt-10">
+        <button className="bg-black hover:bg-blue-700 text-white font-bold py-3 px-10 rounded">
+          Load More
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default CardList
+export default CardList;
