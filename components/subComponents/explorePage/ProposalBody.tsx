32205823a0de888c7bf81f48989533219c3d8825
phaeq <johnnyyin0@gmail.com>
2023-04-26 20:50:59 -0400

    Fixes

--- components/subComponents/explorePage/ProposalBody.tsx
+++ components/subComponents/explorePage/ProposalBody.tsx
@@ -1,27 +1,27 @@
-import React, {useState, useEffect} from 'react'
-import ProposalSearchBar from './ProposalSearchBar';
-import ProposalCardList from './ProposalCardList';
-import ProposalSideBar from './ProposalSideBar';
+import React, { useState, useEffect } from "react";
+import ProposalSearchBar from "./ProposalSearchBar";
+import ProposalCardList from "./ProposalCardList";
+import ProposalSideBar from "./ProposalSideBar";
 
-function ProposalBody({setCurrentPage, jobs, isLoggedIn}) {
-  const [role, setRole] = useState<string>('')
-  const [location, setLocation] = useState<string>('')
-  const [priceRange, setPriceRange] = useState<string>('');
-  const [savedJobs, setSavedJobs] = useState<Array<any>>([])
-  const [appliedJobs, setAppliedJobs] = useState<Array<any>>([])
-  const [filteredJobs, setFilteredJobs] = useState<Array<any>>([])
+function ProposalBody({setCurrentPage,jobs,isLoggedIn}) {
+  const [role, setRole] = useState<string>("");
+  const [location, setLocation] = useState<string>("");
+  const [priceRange, setPriceRange] = useState<string>("");
+  const [filteredJobs, setFilteredJobs] = useState<Array<any>>([]);
+  const [savedJobs, setSavedJobs] = useState<Array<any>>([]);
+  const [appliedJobs, setAppliedJobs] =  useState<Array<any>>([]);
 
   useEffect(() => {
     let filtered = jobs;
     if (role) {
-      filtered = filtered.filter(job => job.roles.includes(role));
+      filtered = filtered.filter((job) => job.roles.includes(role));
     }
     if (location) {
-      filtered = filtered.filter(job => job.locations.includes(location));
+      filtered = filtered.filter((job) => job.locations.includes(location));
     }
     if (priceRange) {
-      const [min, max] = priceRange.split(',');
-      filtered = filtered.filter(job => {
+      const [min, max] = priceRange.split(",");
+      filtered = filtered.filter((job) => {
         const budget = Number(job.budget);
         return budget >= Number(min) && budget <= Number(max);
       });
@@ -31,13 +31,23 @@ function ProposalBody({setCurrentPage, jobs, isLoggedIn}) {
 
   return (
     <div className="w-full mb-20 mx-auto mb-8 max-w-screen-xl">
-      <ProposalSearchBar setRole={setRole} setLocation={setLocation} setPriceRange={setPriceRange}/>
+      <ProposalSearchBar
+        setRole={setRole}
+        setLocation={setLocation}
+        setPriceRange={setPriceRange}
+      />
       <div className="flex">
         <div className="w-3/4 mt-3">
-          <ProposalCardList savedJobs={savedJobs} setCurrentPage={setCurrentPage} filteredJobs={filteredJobs} setSavedJobs={setSavedJobs} setAppliedJobs={setAppliedJobs} isLoggedIn={isLoggedIn}/>
+          <ProposalCardList
+            setSavedJobs={setSavedJobs}
+            setAppliedJobs={setAppliedJobs}
+            setCurrentPage={setCurrentPage}
+            filteredJobs={filteredJobs}
+            isLoggedIn={isLoggedIn}
+          />
         </div>
         <div className="w-1/4 p-3">
-          <ProposalSideBar savedJobs={savedJobs} appliedJobs={appliedJobs}/>
+          <ProposalSideBar savedJobs={savedJobs} appliedJobs={appliedJobs} />
         </div>
       </div>
     </div>
--- components/subComponents/explorePage/ProposalCardList.tsx
+++ components/subComponents/explorePage/ProposalCardList.tsx
@@ -1,26 +1,35 @@
-import React, {useState} from 'react'
-import ProposalCard from "./ProposalCard"
+import React, { useState } from "react";
+import ProposalCard from "./ProposalCard";
 
-function ProposalCardList({setCurrentPage, filteredJobs, setSavedJobs, setAppliedJobs, isLoggedIn, savedJobs}) {
-
+function ProposalCardList({setCurrentPage, filteredJobs, isLoggedIn, setSavedJobs, setAppliedJobs}) {
   const [numToShow, setNumToShow] = useState(4);
 
   const handleLoadMore = () => {
     setNumToShow(numToShow + 4);
-  }
+  };
 
   return (
     <div className="mb-20">
       {filteredJobs.length > 0 ? (
-        filteredJobs.slice(0, numToShow).map((job, index) => (
-          <ProposalCard key={index} job={job} setCurrentPage={setCurrentPage} setSavedJobs={setSavedJobs} setAppliedJobs={setAppliedJobs} isLoggedIn={isLoggedIn}/>
-        ))
+        filteredJobs
+          .slice(0, numToShow)
+          .map((job, index) => (
+            <ProposalCard
+              key={index}
+              job={job}
+              setSavedJobs={setSavedJobs}
+              setAppliedJobs={setAppliedJobs}
+              setCurrentPage={setCurrentPage}
+              isLoggedIn={isLoggedIn}
+            />
+          ))
       ) : (
         <div className="text-center italic mb-4">No jobs found.</div>
       )}
       {filteredJobs.length > numToShow && (
         <div className="flex justify-center mt-10">
-          <button className="bg-black hover:bg-blue-700 text-white font-bold py-3 px-10 rounded"
+          <button
+            className="bg-black hover:bg-blue-700 text-white font-bold py-3 px-10 rounded"
             onClick={handleLoadMore}
           >
             Load More
