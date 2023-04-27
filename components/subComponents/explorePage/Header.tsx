ebea36dbd85e066297c4d18004eff86ed1d53854
phaeq <johnnyyin0@gmail.com>
2023-04-26 22:03:00 -0400

    Changed header

--- components/subComponents/explorePage/Header.tsx
+++ components/subComponents/explorePage/Header.tsx
@@ -1,64 +1,114 @@
-import React, { useState } from 'react';
-import Link from 'next/link';
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "./logo.png";
+import userpic from "./user_default.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRightFromBracket,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
-function Header(props: { 
-  setPage: Function, 
-  page: number, 
-  isLoggedIn:Boolean, 
-  setIsLoggedIn:Function, 
-  setCurrentPage:Function,
-  userInfo:any
+function Header(props: {
+  setPage: Function;
+  page: number;
+  isLoggedIn: Boolean;
+  setIsLoggedIn: Function;
+  setCurrentPage: Function;
+  userInfo: any;
 }) {
-  const { setPage, page, isLoggedIn, setIsLoggedIn, setCurrentPage, userInfo } = props;
+  const { setPage, page, isLoggedIn, setIsLoggedIn, setCurrentPage, userInfo } =
+    props;
+
+  const [showMenu, setShowMenu] = useState(false);
 
   const handleTogglePage = () => {
     setPage(page === 0 ? 1 : 0);
-  }
+  };
 
   const handleSignOut = () => {
-    setIsLoggedIn(false)
-  }
+    setIsLoggedIn(false);
+  };
 
   const handleUserClick = () => {
-    setCurrentPage(3)
-  }
+    setCurrentPage(3);
+  };
 
   return (
-    <div className="mx-auto mb-8 max-w-screen-2xl border-b border-grey-300 px-6 pt-6 pb-6 lg:px-20">
+    <div className="mx-auto mb-8 max-w-screen-2xl border-b border-grey-300 px-6 pt-6 pb-6">
       <div className="title flex flex-row items-center justify-between">
-        <span id="logo" className="title text-4xl font-bold cursor-pointer">HireUp</span> 
-        <div className="relative items-center bg-white md:flex">
-          <div className="flex space-x-4 items-center ml-10">
-            <div>
-              <a
-                className="mx-auto underline mr-5 text-xl hover:text-blue-500 cursor-pointer"
-                onClick={handleTogglePage}
-              >
-                {page === 0 ? 'Find Freelancers' : 'Find Jobs'}
-              </a>
-            </div>
+        <Image src={logo} alt="logo" width="120" height="120" />
+        <div className="flex justify-end items-center bg-white">
+          <div className="flex items-center">
             {isLoggedIn ? (
-              <>
-             <span className="text-xl"> Welcome,{' '}  
-             <span className="hover:text-blue-500 cursor-pointer underline" onClick={handleUserClick}> {userInfo.nickname}</span>
-             !</span>
-              <button className="hover:bg-blue-100 px-10 py-2 border border-solid border-black font-bold rounded-md" onClick={handleSignOut}>
-                Sign Out
-              </button>
-              </>
+              <div className="flex items-center">
+                <a
+                  className="mx-auto underline mr-10 text-xl hover:text-blue-500 cursor-pointer"
+                  onClick={handleTogglePage}
+                >
+                  {page === 0 ? "Find Freelancers" : "Find Jobs"}
+                </a>
+                <img
+                  src={userpic.src}
+                  alt="Profile"
+                  className="rounded-full mr-2 cursor-pointer"
+                  style={{ width: "10%", height: "10%" }}
+                  onClick={() => {
+                    setShowMenu(!showMenu);
+                  }}
+                />
+                <div>
+                  <p className="text-sm font-medium text-gray-900">
+                    {userInfo.nickname}
+                  </p>
+                  <p className="text-sm text-gray-500">Welcome!</p>
+                </div>
+                {showMenu && (
+                  <div
+                    className="absolute mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-50"
+                    style={{ right: "10%", top: "10%" }}
+                  >
+                    <div className="py-1">
+                      <button
+                        className="w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                        onClick={handleUserClick}
+                      >
+                        <span className="mr-2">Profile</span>
+                        <FontAwesomeIcon icon={faUser} />
+                      </button>
+                      <button
+                        className="w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                        onClick={() => {
+                          setIsLoggedIn(false);
+                          setShowMenu(false);
+                        }}
+                      >
+                        <span className="mr-2">Logout</span>
+                        <FontAwesomeIcon icon={faArrowRightFromBracket} />
+                      </button>
+                    </div>
+                  </div>
+                )}
+              </div>
             ) : (
-              <>
-              <Link href="/login">
-              <button className="hover:bg-blue-100 px-10 py-2 border border-solid border-black font-bold rounded-md">
-                  Log In
-                </button>
-              </Link>
-              <Link href="/register">
-              <button className="hover:bg-blue-500 px-20 py-2 bg-black text-white font-bold rounded-md">
-                  Sign Up
-                </button>
-              </Link>
-              </>
+              <div className="flex items-center p-3">
+                <a
+                  className="mx-auto underline mr-10 text-xl hover:text-blue-500 cursor-pointer"
+                  onClick={handleTogglePage}
+                >
+                  {page === 0 ? "Find Freelancers" : "Find Jobs"}
+                </a>
+                <Link href="/login">
+                  <button className="bg-green-200 hover:bg-green-400 text-white rounded-md py-2 px-4 mr-2 shadow-lg">
+                    Login
+                  </button>
+                </Link>
+                <Link href="/register">
+                  <button className="bg-green-300 hover:bg-green-500 text-white rounded-md py-2 px-4 shadow-lg">
+                    Sign Up
+                  </button>
+                </Link>
+              </div>
             )}
           </div>
         </div>
