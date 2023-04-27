50a5601dd417dc945b085fd4bc7ecbbea61dd1a2
Lizz Mullowney <elizabethmullowney@gmail.com>
2023-04-27 16:10:06 -0500

    create UserIdContext at index level with userid of currently logged in user

--- server/models/usersModel.js
+++ server/models/usersModel.js
@@ -2,8 +2,6 @@ const db = require('../db');
 
 const usersModel = {
   createUser: async ( {user}) => {
-    console.log('in usersModel, this is the user object', user);
-
     var name = user.nickname;
     var email = user.email;
     var s_id = user.sid;
@@ -12,17 +10,24 @@ const usersModel = {
 
     const values = [s_id, name, email]
     //do I need to add logic here for if user does not exist
-    const statement = 'INSERT INTO users (s_id, name, email) VALUES ($1, $2, $3) ON CONFLICT (email) DO NOTHING';
+    const statement = `INSERT INTO users (s_id, name, email)
+    VALUES ($1, $2, $3)
+    ON CONFLICT (email)
+    DO UPDATE SET s_id = EXCLUDED.s_id, name = EXCLUDED.name
+    RETURNING id`;
 
-    await client.query(statement, values);
-  },
-  getAll: (id) => {
+
+    const result = await client.query(statement, values);
+    const userId = result.rows[0].id;
+    console.log(userId);
+    return userId;
+  }  getAll: (id) => {
     return db.query('SELECT * FROM users')
   },
   getOne: async (id) => {
     const message = await db.query('SELECT * FROM users WHERE id = $1', [id]);
     return message.rows[0];
   },
-}
+
 
 module.exports = usersModel;
