7fb3739450adaf317e3d76b6ca6b73a794e32acf
Brennin <bkjoiner1@gmail.com>
2023-04-28 10:26:57 -0400

    user search fix

--- components/chat.tsx
+++ components/chat.tsx
@@ -56,7 +56,7 @@ function Chat({sendTo, setState}) {
   return (
     <>
       {newChat ? (
-        <NewChat sendTo={null} setState={setState}/>
+        <NewChat sendTo={0} setState={setState}/>
       ) : (
         <div className="absolute flex items-center justify-center h-[100%] w-[100%] z-50">
         <div className="relative rounded-lg bg-slate-300 flex flex-col items-center justify-center h-[80%] w-[50%] border-2 border-dollar">
